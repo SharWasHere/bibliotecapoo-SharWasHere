076bbe55024430e635aa34ef6fc7051b248861aa
SharWasHere <159039235+SharWasHere@users.noreply.github.com>
2024-04-25 10:33:51 -0500

    Update Biblioteca.java
    añado codigo en biblioteca pendiente por terminar

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -1,5 +1,28 @@
 package com.mycompany.bibliotecapoo;
 
+package com.mycompany.bibliotecapoo;
+
+import java.util.LinkedList;
 public class Biblioteca {
-    // TODO: Aquí va tu código
+    
+   private LinkedList<Libro> libros;
+   
+   public Biblioteca(){
+       this.libros=new LinkedList<>();      
+   }
+   
+   public void registrarLibro (Libro libroNuevo){
+       libros.add(libroNuevo);
+   }
+   
+   public void buscarLibro(String palabraBusqueda){
+       for(int i=0;i<libros.size();i++){
+           Libro libroVisitado=libros.get(i);
+           
+           if(libroVisitado.getTitulo().equals(palabraBusqueda) || libroVisitado.getAutor().equals(palabraBusqueda) || libroVisitado.getGenero().equals(palabraBusqueda)){
+               
+               
+           }
+       }
+   }
 }
