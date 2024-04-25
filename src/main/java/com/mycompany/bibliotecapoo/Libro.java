809c98a8904a45bd4533bc645b5c4dfe9305c528
SharWasHere <159039235+SharWasHere@users.noreply.github.com>
2024-04-25 10:30:06 -0500

    Update Libro.java
    agrego codigo en Libro

--- src/main/java/com/mycompany/bibliotecapoo/Libro.java
+++ src/main/java/com/mycompany/bibliotecapoo/Libro.java
@@ -1,5 +1,52 @@
 package com.mycompany.bibliotecapoo;
 
 public class Libro {
-    // TODO: Aquí va tu código
+
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+    private String genero;
+    private boolean leido;
+
+    public Libro(String titulo, String autor, int anioPublicacion, String genero) {
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+        this.genero = genero;
+        this.leido = false;
+
+    }
+
+    public String mostrarInformacion() {
+        String mostrar = titulo + " " + autor + " " + anioPublicacion + " " + genero;
+        return mostrar;
+
+    }
+
+    public void marcarLeido() {
+        leido = true;
+    }
+
+    public boolean esAntiguo() {
+        int diferencia = 2024 - anioPublicacion;
+
+        if (diferencia > 50) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
 }
