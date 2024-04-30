b9c37a24db28ac17841dcb154df3082b5daa9d5b
SharWasHere <159039235+SharWasHere@users.noreply.github.com>
2024-04-30 00:50:07 -0500

    Update Biblioteca.java

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -5,19 +5,22 @@ import java.util.LinkedList;
 public class Biblioteca {
 
     private LinkedList<Libro> libros;
-/**
+
+    /**
      * Complejidad temporal: O(1) Tiempo constante
      */
     public Biblioteca() {
         this.libros = new LinkedList<>();
     }
-/**
+
+    /**
      * Complejidad temporal: O(1) Tiempo constante
      */
     public void registrarLibro(Libro libroNuevo) {
         libros.add(libroNuevo);
     }
-/**
+
+    /**
      * Complejidad temporal: O(1) Tiempo constante
      */
     public void buscarLibro(String palabraBusqueda) {
@@ -30,38 +33,18 @@ public class Biblioteca {
             }
         }
     }
-/**
-     * Complejidad temporal: O(1) Tiempo constante
-     */
+
     public void mostrarLibros() {
-        for (Libro libro : libros) {
-            System.out.println(libro.mostrarInformacion());
+        for (int i = 0; i < libros.size(); i++) {
+            System.out.println(libros.get(i));
         }
     }
-/**
-     * Complejidad temporal: O(1) Tiempo constante
-     */
-    public void mostrarLibrosNoLeidos() {
 
-        boolean hayLibrosNoLeidos = false;
-
-        for (Libro libro : libros) {
-            if (!libro.isLeido()) {
-                if (!hayLibrosNoLeidos) {
-                    System.out.println("Libros no leídos en la biblioteca:");
-                    hayLibrosNoLeidos = true;
-                }
-                System.out.println(libro.mostrarInformacion());
+    public void mostrarLibrosNoLedos() {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).isLeido() == false) {
+                System.out.println(libros.get(i));
             }
         }
     }
-    public void marcarLibroNoLeido(String tituloLibro) {
-    for (Libro libro : libros) {
-        if (libro.getTitulo().equalsIgnoreCase(tituloLibro)) {
-            libro.marcarLeido(); // Llama al método marcarNoLeido() en la clase Libro
-            return; // Sal del método después de marcar el libro
-        }
-    }
 }
-}
-
